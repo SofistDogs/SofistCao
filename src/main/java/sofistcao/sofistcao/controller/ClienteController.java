a266dd8324ff051c860f567fcfd3aa58b0fe490e
uailucasSilva <lucas.humberto@estudante.iftm.edu.br>
2022-12-03 22:04:50 -0300

    inclusão do backend para o cadastro do cliente

--- src/main/java/sofistcao/sofistcao/controller/ClienteController.java
+++ src/main/java/sofistcao/sofistcao/controller/ClienteController.java
@@ -1,17 +1,35 @@
 package sofistcao.sofistcao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import sofistcao.sofistcao.model.Cliente;
+import sofistcao.sofistcao.repository.ClienteRepository;
 
 @Controller
 public class ClienteController {
+
+    @Autowired
+    ClienteRepository repository;
+
     @GetMapping("/cadastroCliente")
-    public String cadastroCliente() {
+    public String exibeFormCadastroCliente(Model model) {
+        model.addAttribute("cliente", new Cliente());
         return "cadastroCliente";
     }
 
+    @PostMapping("/cadastroCliente")
+    public String gravaNovoCliente(Cliente cliente) {       
+        repository.save(cliente);
+        return "redirect:/home";
+    }
+
     @GetMapping("/consultaClientes")
-    public String consultaCliente() {
+    public String consultaCliente(Model model) {
+        model.addAttribute("clientes", repository.findAll());
         return "consultaClientes";
     }
 
--- src/main/java/sofistcao/sofistcao/repository/ClienteRepository.java
+++ src/main/java/sofistcao/sofistcao/repository/ClienteRepository.java
@@ -1,22 +1,36 @@
 package sofistcao.sofistcao.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import sofistcao.sofistcao.model.Cliente;
+
 @Repository
 public class ClienteRepository {
     @Autowired
     JdbcTemplate jdbc;
 
-    /*
-     * 
-     * public List<Cliente> findAll() {
-     * return jdbc.query("SELECT * FROM clientes", this::mapper);
-     * }
-     * 
-     */
+    public void save(Cliente cliente) {
+        String sql = "insert into cliente(NOME_CLIENTE, LOGIN, SENHA) values (?,?,?);";
+        jdbc.update(sql, cliente.getNome_cliente(), cliente.getLogin(), cliente.getSenha());
+    }
+
+    public List<Cliente> findAll() {
+        return jdbc.query("SELECT * FROM cliente", this::mapper);
+    }
+
+    public Cliente findById(Long id) {
+        return jdbc.queryForObject("SELECT * from cliente where id_cliente=?", this::mapper, id);
+    }
+
+    private Cliente mapper(ResultSet registro, int contador) throws SQLException {
+        return new Cliente(registro.getLong("ID_CLIENTE"), registro.getString("NOME_CLIENTE"),
+                registro.getString("LOGIN"), registro.getString("SENHA"));
+    }
 
 }
